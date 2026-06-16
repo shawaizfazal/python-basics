74f76762a1122f49bb47d51ff78eb70f016a7653
shawaizfazal <shazmaxfazal@gmail.com>
2026-06-16 13:17:43 +0500

    python opreators

---
+++ percentage.py
@@ -0,0 +1,8 @@
+print("enter marks obtained in 4 subjects :")
+math=int(input("maths"))
+english= int(input("english"))
+science=int(input("science"))    
+urdu=int(input("urdu")) 
+sum=math+english+science+urdu
+perc=(sum/400)*100
+print("percentage mark=" , perc)       
