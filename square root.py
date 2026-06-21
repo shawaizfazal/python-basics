30a8524aa9dd03b3cee7dec89e1a2cb8a2df52ee
shawaizfazal <shazmaxfazal@gmail.com>
2026-06-21 13:05:13 +0500

    Add files via upload

---
+++ square root.py
@@ -0,0 +1,10 @@
+# Square Root Calculator Project
+
+# asking the user to type a number
+number = float(input("Enter a number to find its square root: "))
+
+# calculating the square root using power of 0.5
+square_root = number ** 0.5
+
+# printing the answer
+print("The square root of", number, "is", square_root)
