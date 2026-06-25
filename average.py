89c6a339978bec532e14a1a1eae574509cbebb32
shawaizfazal <shazmaxfazal@gmail.com>
2026-06-25 13:16:42 +0500

    python challenges

--- average.py
+++ average.py
@@ -1,10 +1,21 @@
-#storing values
-tree1= 98
-tree2= 94
-tree3= 41
-tree4= 95
-tree5= 11
-sum=tree1+tree2+tree3+tree4+tree4
-print("the sum of all the 5 trees is :" , sum)
-average= sum/5
-print("the average of all trees is :" , average)
+a=int(input("enter a value:"))
+b=int(input("enter a valur2:"))
+c=int(input("enter a value3:"))
+avg=(a+b+c)/3
+if avg > a and avg > b and avg > c:
+    print("%d is higher than %d ,%d,%d" % (avg,a,b,c))
+elif avg > a and avg < b:
+   print ("%d is higher than %d,%d" %(avg,a,b))
+elif avg > a and avg >c:
+   print ("%d is higher than %d,%d" %(avg,a,c))
+elif avg > b and avg >c:
+      print ("%d is higher than %d,%d" %(avg,b,c))
+elif avg > a:
+       print ("%d is higher than %d" %(avg,a))
+elif avg > b:
+       print ("%d is higher than %d" %(avg,b))
+
+elif avg > c:
+            print ("%d is higher than %d" %(avg,c))
+else:
+      print("invalid input")
---
+++ opreatorprecedence.py
@@ -0,0 +1,12 @@
+v=4
+w=5
+x=8
+y=2
+z=0
+z=("value of (v+w) * x/y is " , 2)
+name = "alex"
+age=0
+if name =="alex" or name == "john" and age>=2:
+    print("hello! Welcome.")
+else:
+    print("good bye!!")
