d3fb58f03e2a517abcfc7f860b5534641e5618da
shawaizfazal <shazmaxfazal@gmail.com>
2026-06-30 11:47:01 +0500

    variables

---
+++ variable.py
@@ -0,0 +1,14 @@
+# Storing birthdays in 5 different variables
+friend1_birthday = "October 14"
+friend2_birthday = "March 5"
+friend3_birthday = "August 21"
+friend4_birthday = "January 30"
+friend5_birthday = "november 24"
+
+# Printing the birthdays
+print("Raj's Friends' Birthdays:")
+print("Friend 1:", friend1_birthday)
+print("Friend 2:", friend2_birthday)
+print("Friend 3:", friend3_birthday)
+print("Friend 4:", friend4_birthday)
+print("Friend 5:", friend5_birthday)
