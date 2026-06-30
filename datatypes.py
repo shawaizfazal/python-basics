43a505a148c9bf9490f95b4103580c9843f0e11d
shawaizfazal <shazmaxfazal@gmail.com>
2026-06-30 11:32:29 +0500

    Datatypes

---
+++ datatypes.py
@@ -0,0 +1,7 @@
+
+# taking the input from the user 
+msg = input("Enter the congratulations message in small letters: ")
+big_msg = msg.upper()
+
+print("Here is your message in upper case:")
+print(big_msg)
