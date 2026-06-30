34bc1abaf693fa662007b041ba53833300e6d6cc
shawaizfazal <shazmaxfazal@gmail.com>
2026-06-30 11:41:34 +0500

    Add files via upload

---
+++ python routine.py
@@ -0,0 +1,11 @@
+print("welcome to my daily routine")
+#point 1 waking up
+print("i wake up at 5am")
+#point2 going to school
+print("i go to school at 7am")
+#point3 comning back from school
+print("i eat lunch after coming back from school in the afternoon")
+#point 4 studying
+print("i start studying for 3 hours in the evening till 6pm")
+#calculate overall stdying time
+print("total hours studying in a week" ,2*7)
